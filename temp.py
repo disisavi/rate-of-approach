66ccc2f0cb3f2952d43e394d13cc2996402b0f69
Abhijeet Mishra <dsisavi@gmail.com>
2019-04-17 14:13:42 -0400

    1. Area calculation vectorised 2. contour detection improved

--- temp.py
+++ temp.py
@@ -3,24 +3,35 @@ import cv2
 import matplotlib.pyplot as plt
 
 def area(vs):
-    a = 0
-    x0,y0 = vs[0]
-    for [x1,y1] in vs[1:]:
-        dx = x1-x0
-        dy = y1-y0
-        a += 0.5*(y0*dx - x0*dy)
-        x0 = x1
-        y0 = y1
-    return a
+        x=vs[:,0]
+        y=vs[:,1]
+        area=0.5*np.sum(y[:-1]*np.diff(x) - x[:-1]*np.diff(y))
+        return np.abs(area)
+
+
+def showImage(frame):
+        cv2.imshow('image',frame)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
 
 def getCountours(frame):
-    imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    ret, thresh = cv2.threshold(imgray, 200, 255, 0)
+        print("starting")
+        frame = cv2.GaussianBlur(frame,(9,9),0)
+        showImage(frame)
+        print("meanShiftTime")
+        edge = cv2.Canny(frame,100,200)
+        showImage(edge)
+        # imgray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        ret, thresh = cv2.threshold(edge, 2, 255, 0)
+        contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        for (i, c) in enumerate(contours):
+                tl = c[:,0,:] 
+                print("i = ",i,"  ",tl.shape, "- Area -- ", area(tl))
 
-    contours, _ = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-    for (i, c) in enumerate(contours):
-        print(c.shape)
-
+        
+        print(len(contours))
+        cv2.drawContours(frame, contours, -1, (0,255,0), 2)
+        showImage(frame)
 
 
 image = cv2.imread("test.jpg")
