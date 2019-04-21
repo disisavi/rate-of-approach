8873db276687d8b5e7b6ed0b0e19b851b295cd86
disisavi <disisavi@github.com>
2019-04-21 01:02:09 -0400

    1. Object detection for video stream along with controur calculation... Better contour calculation available at branh SA for now

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # rate-of-approach
 This is the repository for collaboration on lane detection and ROA computation.
+
+
+## YOLO
+ Download the pre-trained YOLO v3 weights file from this [link](https://pjreddie.com/media/files/yolov3.weights) and place it in the current directory or you can directly download to the directory above the current in terminal using
+ 
+ `$ wget -P ../ https://pjreddie.com/media/files/yolov3.weights` 
--- objectYolo.py
+++ objectYolo.py
@@ -1,1 +1,113 @@
-print("Hello Workd")
+import cv2 as cv
+import cv2
+import numpy as np
+
+COLORS = None
+classes = None
+
+
+def get_output_layers(net):
+    layer_names = net.getLayerNames()
+
+    output_layers = [layer_names[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
+    return output_layers
+
+
+def draw_prediction(img, class_id, confidence, x, y, x_plus_w, y_plus_h):
+    label = str(classes[class_id])
+    if (label == 'car'):
+        color = COLORS[class_id]
+
+        cv2.rectangle(img, (x, y), (x_plus_w, y_plus_h), color, 2)
+
+        cv2.putText(img, label, (x - 10, y - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.5, color, 2)
+    # if (label == "car"):
+    #     imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    #     imm = imgray[y:y_plus_h, x:x_plus_w]
+    #     im = np.zeros_like(imm)
+    #     ret, thresh = cv.threshold(imm, 140, 255, 0)
+    #     contours, hierarchy = cv.findContours(thresh, cv.RETR_EXTERNAL, cv.CHAIN_APPROX_SIMPLE)
+    #     cont = cv.drawContours(im, contours, -1, (255, 255, 255), 1)
+    # cv2.imshow("im",im)
+    # cv2.waitKey(0)
+
+
+def getObject(image, net, scale):
+    Width = image.shape[1]
+    Height = image.shape[0]
+
+    blob = cv2.dnn.blobFromImage(image, scale, (416, 416), (0, 0, 0), True, crop=False)
+    net.setInput(blob)
+    outs = net.forward(get_output_layers(net))
+    class_ids = []
+    confidences = []
+    boxes = []
+    conf_threshold = 0.5
+    nms_threshold = 0.4
+
+    for out in outs:
+        for detection in out:
+            scores = detection[5:]
+            class_id = np.argmax(scores)
+            confidence = scores[class_id]
+            if confidence > 0.5:
+                center_x = int(detection[0] * Width)
+                center_y = int(detection[1] * Height)
+                w = int(detection[2] * Width)
+                h = int(detection[3] * Height)
+                x = center_x - w / 2
+                y = center_y - h / 2
+                class_ids.append(class_id)
+                confidences.append(float(confidence))
+                boxes.append([x, y, w, h])
+
+    indices = cv2.dnn.NMSBoxes(boxes, confidences, conf_threshold, nms_threshold)
+
+    for i in indices:
+        i = i[0]
+        box = boxes[i]
+        x = box[0]
+        y = box[1]
+        w = box[2]
+        h = box[3]
+        draw_prediction(image, class_ids[i], confidences[i], round(x), round(y), round(x + w), round(y + h))
+
+    cv2.imshow("object detection", image)
+
+
+def main():
+    global COLORS
+    location = '../BDDA/test/camera_videos/526.mp4'
+    args_config = './yolov3.cfg'
+    args_weights = '../yolov3.weights'
+    args_classes = './yolov3.txt'
+
+    scale = 0.00392
+    global classes
+
+    with open(args_classes, 'r') as f:
+        classes = [line.strip() for line in f.readlines()]
+
+    COLORS = np.random.uniform(0, 255, size=(len(classes), 3))
+
+    net = cv2.dnn.readNet(args_weights, args_config)
+
+    cap = cv2.VideoCapture(location)
+    # cap = cv2.VideoCapture(0)
+    if (cap.isOpened() == False):
+        print("Error opening video stream or file")
+    while (cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == True:
+            getObject(frame, net, scale)
+            if cv2.waitKey(25) & 0xFF == ord('q'):
+                break
+        else:
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+
+if __name__ == "__main__": main()
